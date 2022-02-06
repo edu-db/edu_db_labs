25ace0ecf9867404ed2a3ec941d1ccb5bba2488e
Ivan Lazariev <lazariev.ivan02@gmail.com>
2022-02-06 19:39:17 +0200

    Fix

--- publish.sh
+++ publish.sh
@@ -10,12 +10,12 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # deploying to a custom domain
-echo 'db_project.local' > CNAME
+echo 'dbproject.local' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/LazarievIvan/project_control_system master:gh-pages
+git push -f https://github.com/LazarievIvan/project_control_system.git master:gh-pages
 
 cd -
