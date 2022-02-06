647b1360f0185add9d4198f68408258ccfc9396b
Lazariev Ivan <78935308+LazarievIvan@users.noreply.github.com>
2022-02-06 19:21:55 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -9,17 +9,13 @@ npm run docs:build
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# deploying to a custom domain
+echo 'db_project.local' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:boldak/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/edu-db/edu_db_labs master:gh-pages
+git push -f https://github.com/LazarievIvan/project_control_system master:gh-pages
 
 cd -
