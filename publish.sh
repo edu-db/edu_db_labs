a8d2341c6fcdc44bfde1c9059295b7837a3d7919
TheDanielll <99608245+TheDanielll@users.noreply.github.com>
2022-05-18 04:06:42 +0300

    deploy

--- publish.sh
+++ publish.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/TheDanielll/edu_db_labs/dis-edu.git master:gh-pages
+git push -f https://thedanielll.github.io/edu_db_labs/
+
 
 cd -
