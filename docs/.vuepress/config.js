a6fa1b0bea895f9db7d3b6c609ac838e2486e04e
Dmytro Klymchuk <dima.klimchuk.2016@gmail.com>
2023-02-16 18:39:06 +0200

    initial commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,82 +1,91 @@
 // .vuepress/config.js
 module.exports = {
   plugins: [
-    '@vuepress/back-to-top', 
-    '@vuepress/pwa', 
+    '@vuepress/back-to-top',
+    '@vuepress/pwa',
     {
       serviceWorker: true,
-      updatePopup: true
+      updatePopup: true,
     },
     ,
-    ['container', {
-      type: 'vue',
-      before: '<pre class="vue-container"><code>',
-      after: '</code></pre>'
-    }]
+    [
+      'container',
+      {
+        type: 'vue',
+        before: '<pre class="vue-container"><code>',
+        after: '</code></pre>',
+      },
+    ],
   ],
   port: 3030,
-  base: '/edu_db_labs/',
+  base: '/edu_db_labs_IO-12_Team-1/',
   theme: 'cool',
   // dest: 'dist',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css' }],
-    ['link', {href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel :'stylesheet'}],
-    
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css',
+      },
+    ],
+    [
+      'link',
+      {
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+        rel: 'stylesheet',
+      },
+    ],
   ],
   themeConfig: {
     // logo: './myAvatar.png',
     sidebar: [
-    {
-      title: 'Вступ',
-      path:"/intro/"
-    },
-    {
-      title: 'Розроблення загальних вимог до системи',
-        path:"/requirements/",
-        children:[
+      {
+        title: 'Вступ',
+        path: '/intro/',
+      },
+      {
+        title: 'Розроблення загальних вимог до системи',
+        path: '/requirements/',
+        children: [
           '/requirements/state-of-the-art',
           '/requirements/stakeholders-needs',
-        ]
-      
-    },
+        ],
+      },
 
-    {
-      title: 'Розроблення вимог до функціональности системи',
-      path:"/use cases/"
-    },
+      {
+        title: 'Розроблення вимог до функціональности системи',
+        path: '/use cases/',
+      },
 
-    {
-      title: 'Проектування інформаційного забезпечення',
-      path:"/design/"
-    },
+      {
+        title: 'Проектування інформаційного забезпечення',
+        path: '/design/',
+      },
 
-    {
-      title: 'Реалізація інформаційного та програмного забезпечення',
-      path:"/software/"
-    },
+      {
+        title: 'Реалізація інформаційного та програмного забезпечення',
+        path: '/software/',
+      },
 
-    {
-      title: 'Тестування працездатності системи',
-      path:"/test/"
-    },
-    {
-      title: 'Висновки',
-      path:"/conclusion/"
-    }
-
-  ],
+      {
+        title: 'Тестування працездатності системи',
+        path: '/test/',
+      },
+      {
+        title: 'Висновки',
+        path: '/conclusion/',
+      },
+    ],
     sidebarDepth: 2,
     displayAllHeaders: true, // Default: false
 
-
-    nav: [
-      { text: 'Початок', link: '/' },
-    ],
+    nav: [{ text: 'Початок', link: '/' }],
 
     lastUpdated: 'Останнє оновлення', // string | boolean
-      // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/edu-db/edu_db_labs',
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'https://github.com/DimaKlimchuk/edu_db_labs_IO-12_Team-1',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
@@ -93,23 +102,22 @@ module.exports = {
     // editLinks: true,
     // custom text for edit link. Defaults to "Edit this page"
     // editLinkText: 'Ви можете покращити цю сторінку'
-
   },
-  title: 'Назва проєкту',
+  title: 'Система організації та управління опитуваннями експертів',
   description: 'Лабораторні роботи з дисципліни "Організація баз даних"',
   configureWebpack: {
     resolve: {
       alias: {
-        '@alias': '../img'
-      }
-    }
+        '@alias': '../img',
+      },
+    },
   },
   markdown: {
-    extendMarkdown: md => {
-      md.set({ html: true })
-      md.use(require('markdown-it-katex'))
-      md.use(require('markdown-it-plantuml'))
-      md.use(require('markdown-it-admonition'))
-    }
-  }
-}
+    extendMarkdown: (md) => {
+      md.set({ html: true });
+      md.use(require('markdown-it-katex'));
+      md.use(require('markdown-it-plantuml'));
+      md.use(require('markdown-it-admonition'));
+    },
+  },
+};
--- docs/README.md
+++ docs/README.md
@@ -3,18 +3,28 @@ home: true
 actionText: Розпочати →
 actionLink: /intro/
 
-footer: "ECL 2.0 Licensed | Copyright © [YYYY] [Your Name]"
+footer: 'ECL 2.0 Licensed | Copyright © [YYYY] [Your Name]'
 ---
 
+**Виконали:**
 
-**Виконав(-ла):** 
+**студенти 2-го курсу, групи ІО-12**
 
-*студент(-ка) 2-го курсу, групи (шифр групи)*<span padding-right:5em></span> **[ім’я ПРІЗВИЩЕ] [Посилання email, tg, fb]**
+<span padding-right:5em></span> **Дмитро Климчук** <a href="https://t.me/Dmytro_Klymchuk" target="_blank">Telegram</a>
 
+<span padding-right:5em></span> **Дмитро Лелюх** <a href="https://t.me/Aerowskiy" target="_blank">Telegram</a>
+
+<span padding-right:5em></span> **Василь Мамчур** <a href="https://t.me/vasya_mamchur" target="_blank">Telegram</a>
+
+<span padding-right:5em></span> **Микита Зеленський** <a href="https://t.me/Pium_Peccator" target="_blank">Telegram</a>
+
+<span padding-right:5em></span> **Артем Тугарінов** <a href="https://t.me/Art1213" target="_blank">Telegram</a>
+
+<span padding-right:5em></span> **Ієрємія Пінчук** <a href="https://t.me/Hoisasa" target="_blank">Telegram</a>
 
 **Керівник**
 
-*доцент кафедри ОТ ФІОТ, к.т.н., доцент*<span padding-right:5em></span> **Андрій БОЛДАК** 
+_доцент кафедри ОТ ФІОТ, к.т.н., доцент_<span padding-right:5em></span> **Андрій БОЛДАК**
 
 [НТУУ "КИЇВСЬКИЙ ПОЛІТЕХНІЧНИЙ ІНСТИТУТ імені ІГОРЯ СІКОРСЬКОГО](https://kpi.ua/)
 
