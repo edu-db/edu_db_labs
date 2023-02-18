9ba9ead21ddebb66f1ee2f425b2f7667fbf04d4e
shunichkaaaa <gdaria296@gmail.com>
2023-02-18 17:22:05 +0200

    comments added to publish.bat

--- publish.bat
+++ publish.bat
@@ -1,11 +1,21 @@
+:: build
 npm run docs:build
 
+:: navigate into the build output directory
 cd docs/.vuepress/dist
+
+:: if you are deploying to a custom domain
+:: echo 'www.example.com' > CNAME
+
 
 git init
 git add -A
 git commit -m 'deploy'
 
+:: if you are deploying to https://<USERNAME>.github.io
+:: git push -f git@github.com:boldak/<USERNAME>.github.io.git master
+
+:: if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/shunichkaaaa/edu_db_labs_IO-12_Group-4 master:gh-pages
 
 cd ..\..\..
